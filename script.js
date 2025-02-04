d118cfe4fa453ce0ea3a5d1e0667d51841d34c77
prithee <ganesanpritheekha@gmail.com>
2025-02-04 01:54:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ const messages = [
 let messageIndex = 0;
 
 function handleNoClick() {
-    const noButton = document.querySelector('.no-button');
-    const yesButton = document.querySelector('.yes-button');
-    noButton.textContent = messages[messageIndex];
+    const nawButton = document.querySelector('.naw-button');
+    const YAZZButton = document.querySelector('.YAZZ-button');
+    nawButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
-    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+    const currentSize = parseFloat(window.getComputedStyle(YAZZButton).fontSize);
+    YAZZButton.style.fontSize = `${currentSize * 1.5}px`;
 }
 
 function handleYesClick() {
-    window.location.href = "yes_page.html";
+    window.location.href = "YAZZ_page.html";
 }
